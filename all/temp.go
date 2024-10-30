e697b811057240d5c70fe5c3e1c8855671aa2e5d
yanghuangzheng <hhh2209443117@163.com>
2024-10-30 13:54:35 +0800

    Update temp.go

--- all/temp.go
+++ all/temp.go
@@ -4,3 +4,4 @@ https://github.com/protocolbuffers/protobuf/releases
 protoc -I . goods.proto --go_out=plugins=grpc:.
 protoc -I . helloworld.proto --go_out=plugins=grpc:.
 protoc -I . helloworld.proto --go_out=. --go-grpc_out=.
+protobuf的原理
