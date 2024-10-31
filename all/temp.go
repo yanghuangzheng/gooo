39c67acb34a2aed8f4795f5da8a25ab7eb2e08f4
yanghuangzheng <hhh2209443117@163.com>
2024-10-31 16:13:29 +0800

    Update temp.go

--- all/temp.go
+++ all/temp.go
@@ -4,6 +4,7 @@ https://github.com/protocolbuffers/protobuf/releases
 protoc -I . goods.proto --go_out=plugins=grpc:.
 protoc -I . helloworld.proto --go_out=plugins=grpc:.
 protoc -I . helloworld.proto --go_out=. --go-grpc_out=.
+protoc -I . --go_out=plugins=grpc:. --validate_out="lang=go:." form.proto
 "google.golang.org/grpc/metadata"
 go-grpc-middleware
 "google.golang.org/grpc/status"
