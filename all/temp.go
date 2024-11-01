1930e62a70b30b02407cb2b59402aeede7608e57
yanghuangzheng <hhh2209443117@163.com>
2024-11-01 09:12:20 +0800

    Update temp.go

--- all/temp.go
+++ all/temp.go
@@ -10,4 +10,37 @@ go-grpc-middleware
 "google.golang.org/grpc/status"
 google.golang.org/grpc/codes
 
-
+OK (0)
+成功完成。
+CANCELLED (1)
+操作被取消（通常是客户端取消请求）。
+UNKNOWN (2)
+未知错误。通常表示发生了意外错误。
+INVALID_ARGUMENT (3)
+客户端提供了无效的参数。
+DEADLINE_EXCEEDED (4)
+请求超时。
+NOT_FOUND (5)
+请求的资源未找到。
+ALREADY_EXISTS (6)
+请求的资源已存在。
+PERMISSION_DENIED (7)
+客户端没有足够的权限执行请求的操作。
+RESOURCE_EXHAUSTED (8)
+资源耗尽（例如，配额或限制）。
+FAILED_PRECONDITION (9)
+操作失败，因为前提条件不满足。
+ABORTED (10)
+操作被中止，通常是因为并发操作导致的冲突。
+OUT_OF_RANGE (11)
+操作失败，因为索引或键超出范围。
+UNIMPLEMENTED (12)
+服务未实现请求的方法。
+INTERNAL (13)
+内部错误。通常表示服务器内部发生了错误。
+UNAVAILABLE (14)
+服务不可用，通常是由于维护或过载。
+DATA_LOSS (15)
+发生数据丢失或损坏。
+UNAUTHENTICATED (16)
+请求未通过身份验证。
