8ae1cd6a31563408e96aac647e26dc7cc593a9cd
yanghuangzheng <hhh2209443117@163.com>
2024-11-04 17:32:22 +0800

    Update temp.go

--- all/temp.go
+++ all/temp.go
@@ -57,153 +57,93 @@ go get -u gorm.io/gorm
 go get -u gorm.io/driver/mysql
 
 https://gorm.io/zh_CN/docs/logger.html
-/////////////////////////////////////////////////////////
-err_resolve
-////////////////////////////////////////////////////////
-client
-///////////////////////////////////////////////////////////
+///////////////////////////////////////////////
+proto
+///////////////////////////////////////////
+main
+//////////////////////////////////////////
 package main
-import(
-	"context"
-	"fmt"
 
-	"all/grpc_test/proto"
-
-	"google.golang.org/grpc"
-	"google.golang.org/grpc/status"
-	//"google.golang.org/grpc/codes"
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+	"all/gin/proto/proto"
 )
-func main(){
-	 conn,err:=grpc.Dial("127.0.0.1:8080",grpc.WithInsecure())
-	 if err!=nil{
-		 panic(err)
-	 }
-	 defer conn.Close()
-	 
-	 c:=proto.NewGreeterClient(conn)
-	 _,err=c.SayHello(context.Background(),&proto.HelloRequest{Name:"bobby"})
-	 if err!=nil{
-		//panic(err)
-		st,ok := status.FromError(err)
-		if !ok{
-                 panic("解析error错误")
-		}
-		fmt.Println(st.Message())
-		fmt.Println(st.Code())
+
+func createGoods(c *gin.Context) {
+	var mes struct{
+		Name string `json:"user"`
+		Mess string
+		Number int
 	}
-	//fmt.Println(r.Message)
+	mes.Name="bobby"
+	mes.Mess="测试mes"
+	mes.Number=3
+	c.JSON(http.StatusOK, mes)
 }
-//////////////////////////////////////////////////////////////
-service
-/////////////////////////////////////////////////////////////////
-package main
-import(
-	"context"
-	"net"
-	//"time"
-
-	"all/grpc_test/proto"
-
-	"google.golang.org/grpc"
-	//"google.golang.org/grpc/status"
-	//"google.golang.org/grpc/codes"
-)
-type Server struct{
-	//proto.UnimplementedGreeterServer
-}
-//ctx主要解决协程超时
-func (s *Server) SayHello(ctx context.Context,in*proto.HelloRequest) (*proto.HelloReply, error) {
-	//返回错误代码
-	//return nil,status.Errorf(codes.NotFound,"记录未找到：%s",in.Name)
-	//time.Sleep(5*time.Second)
-   return &proto.HelloReply{
-        Message :"hello hhh" + in.Name,
-    }, nil
-}
-func main(){
-	g:=grpc.NewServer()
-	//var serverGreeter = &Server{}
-	proto.RegisterGreeterServer(g,&Server{})
-	lis,err:=net.Listen("tcp","0.0.0.0:8080")
-	if err!=nil {
-		panic("failed to listen:"+err.Error())
+func createproto(c *gin.Context) {
+	course:=[]string{"python","go","微服务"}
+	user := proto.Teacher{
+        Name:"bobby",
+		Course:course,
 	}
-	err=g.Serve(lis)
+	c.ProtoBuf(http.StatusOK, user)
 }
-////////////////////////////////////////////////////////////////////////
-sql
-////////////////////////////////////////////////////////////////////////
-main.go
-//////////////////////////////////////////////////////////////////////
+func main() {
+	// 实例化一个 gin 的 server 对象
+	r := gin.Default() // 默认开启 logger 日志和 recovery 返回状态中间件
+	// 定义分组路由
+		good := r.Group("/goods")
+	{
+		good.GET("/morejson", createGoods)
+		good.GET("/moreproto", createproto)
+		
+	}
+
+	// 启动服务器
+	r.Run(":8083")
+}
+///////////////////////////////
+.proto
+//////////////////////////////////
+syntax="proto3";
+option go_package=".;proto";
+message Teacher{
+    string name=1;
+    repeated string course=2;
+}
+////////////////////////////////
+json
+/////////////////////////////////
 package main
 
-import(
-	"log"
-	"os"
-	"time"
-
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
-	"database/sql"
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
 )
 
-type Product struct {
-	gorm.Model//一些默认字段
-	Code  sql.NullString//通过nullstring来设置零值问题
-	Price uint
-  }
-func main()  {
-	dsn := "sql路径"
-
-
-	newLogger := logger.New(
-		log.New(os.Stdout, "\r\n", log.LstdFlags), // io writer
-		logger.Config{
-		  SlowThreshold:              time.Second,   // Slow SQL threshold
-		  LogLevel:                   logger.Info, // Log level
-		  IgnoreRecordNotFoundError: true,           // Ignore ErrRecordNotFound error for logger
-		  ParameterizedQueries:      true,           // Don't include params in the SQL log
-		  Colorful:                  true,          // Disable color
-		},)
-
-
-		db,err := gorm.Open(mysql.Open(dsn), &gorm.Config{
-			Logger: newLogger,
-		  })
-		  if err!=nil{
-			panic(err)
-		}
-
-	  // 迁移 schema
-  db.AutoMigrate(&Product{})//定义一个表结构将表结构直接生成对应的表 -migrations实例化一个空表此处一个有sql语句
-  // Create
-  //db.Create(&Product{Code:sql.NullString{"D42",true}, Price:100})//创建一个例子
-  product := Product{
-    Code:  sql.NullString{String: "D42", Valid: true},
-    Price: 100,
+func createGoods(c *gin.Context) {
+	var mes struct{
+		Name string `json:"user"`
+		Mess string
+		Number int
+	}
+	mes.Name="bobby"
+	mes.Mess="测试mes"
+	mes.Number=3
+	c.JSON(http.StatusOK, mes)
 }
-db.Create(&product)
-  // Read
-  //var product Product
-  db.First(&product, 1) // 根据整型主键查找
-  db.First(&product, "code = ?", "D42") // 查找 code 字段值为 D42 的记录
-  // Update - 将 product 的 price 更新为 200
-  db.Model(&product).Update("Price", 200)
-  // Update - 更新多个字段
-  db.Model(&product).Updates(Product{Price: 200, Code:sql.NullString{String: "D42", Valid: true}}) // 仅更新非零值字段 将code“”写入只是code没有改变将price设为0则price还是没有改变
-  //如果我们去更新一个produce 只设置了price200 会导致其他值设为默认值
-  //利用nullstring
-  product= Product{
-    Code:  sql.NullString{"",true},
-    Price: 100,
+func main() {
+	// 实例化一个 gin 的 server 对象
+	r := gin.Default() // 默认开启 logger 日志和 recovery 返回状态中间件
+	// 定义分组路由
+		good := r.Group("/goods")
+	{
+		good.GET("/morejson", createGoods)
+		good.POST("/add", createGoods)
+		
+	}
+
+	// 启动服务器
+	r.Run(":8083")
 }
-  db.Model(&product).Updates(&product)
-  db.Model(&product).Updates(map[string]interface{}{"Price": 200, "Code": "F42"})
-  // Delete - 删除 product
-  db.Delete(&product, 1)
-}
-	  // Globally mode
-	  
-//设置全局的logger，这个logger在我们只想每个sql语句时候会打印一行sql
-//sql是最重要的
